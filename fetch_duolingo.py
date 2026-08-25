e6e73e1a9a3e142c1368c01d4fc5ac5a19d2be66
Sudarshana A D <sudarshanaad2112@gmail.com>
2026-08-25 16:22:55 +0530

    Update fetch_duolingo.py

--- fetch_duolingo.py
+++ fetch_duolingo.py
@@ -8,7 +8,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 }
 
-url = f"https://duolingo.com{DUOLINGO_USERNAME}"
+url = f"https://duolingo.com/{DUOLINGO_USERNAME}"
 print(f"[DEBUG 1] Attempting to reach URL: {url}")
 
 response = requests.get(url, headers=headers)
